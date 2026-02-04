32f0da37bf8d4d79ba4a285cc3a0c99f5657978c
LSH <lsh2613@naver.com>
2026-02-04 15:40:07 +0900

    init

---
+++ src/main/kotlin/dev/lsh2613/Lsh2613Application.kt
@@ -0,0 +1,11 @@
+package dev.lsh2613
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lsh2613Application
+
+fun main(args: Array<String>) {
+	runApplication<Lsh2613Application>(*args)
+}
---
+++ src/test/kotlin/dev/lsh2613/Lsh2613ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.lsh2613
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lsh2613ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
