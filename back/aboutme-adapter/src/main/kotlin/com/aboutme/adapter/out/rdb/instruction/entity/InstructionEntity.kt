e9e6e0ef220e8eb99a6a3538fbaedd0ed99eb21b
LSH <lsh2613@naver.com>
2026-03-11 17:47:20 +0900

    feat(back): instruction 엔티티 구현

---
+++ back/aboutme-adapter/src/main/kotlin/com/aboutme/adapter/out/rdb/instruction/entity/InstructionEntity.kt
@@ -0,0 +1,62 @@
+package com.aboutme.adapter.out.rdb.instruction.entity
+
+import com.aboutme.adapter.out.rdb.common.converter.StringListConverter
+import com.aboutme.adapter.out.rdb.common.entity.DateAuditable
+import jakarta.persistence.Column
+import jakarta.persistence.Convert
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Table(name = "instruction")
+@Entity
+class InstructionEntity(
+    profileImageUrl: String? = null,
+    name: String,
+    emails: List<String>,
+    region: String,
+    education: String,
+    skills: List<String>,
+) : DateAuditable() {
+    companion object {
+        const val SINGLETON_ID = 0L
+    }
+
+    fun update(
+        name: String,
+        emails: List<String>,
+        region: String,
+        education: String,
+        skills: List<String>,
+        profileImageUrl: String?,
+    ) {
+        this.name = name
+        this.emails = emails
+        this.profileImageUrl = profileImageUrl
+        this.region = region
+        this.education = education
+        this.skills = skills
+    }
+
+    @Id
+    val id: Long = SINGLETON_ID
+
+    var profileImageUrl: String? = profileImageUrl
+        protected set
+    var name: String = name
+        protected set
+
+    @Convert(converter = StringListConverter::class)
+    @Column(columnDefinition = "VARCHAR")
+    var emails: List<String> = emails
+        protected set
+    var region: String = region
+        protected set
+    var education: String = education
+        protected set
+
+    @Convert(converter = StringListConverter::class)
+    @Column(columnDefinition = "VARCHAR")
+    var skills: List<String> = skills
+        protected set
+}
