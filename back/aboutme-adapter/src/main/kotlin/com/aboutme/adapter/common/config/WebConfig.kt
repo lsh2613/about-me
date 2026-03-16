57acba0d76f787cac6a601a044821f868ba42087
LSH <lsh2613@naver.com>
2026-03-16 17:08:43 +0900

    feat(back): 정적 리소스 캐시 정책 적용

--- back/aboutme-adapter/src/main/kotlin/com/aboutme/adapter/common/config/WebConfig.kt
+++ back/aboutme-adapter/src/main/kotlin/com/aboutme/adapter/common/config/WebConfig.kt
@@ -2,8 +2,10 @@ package com.aboutme.adapter.common.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.time.Duration
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
@@ -12,6 +14,11 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("$uploadBasePath/**")
-            .addResourceLocations("file:$uploadBasePath/")
+            .addResourceLocations("file:$uploadBasePath")
+            .setCacheControl(cacheControl())
+    }
+
+    fun cacheControl(): CacheControl {
+        return CacheControl.maxAge(Duration.ofHours(1)).cachePublic()
     }
 }
