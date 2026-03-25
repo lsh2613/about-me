ee07b2f6e17e2fa7ca4c93af66fc454c30ecc090
LSH <lsh2613@naver.com>
2026-03-25 23:49:21 +0900

    refactor(back): CommonErrorCode 파일 위치 변경

--- back/aboutme-app/src/main/kotlin/com/aboutme/app/common/util/CommonValidationUtil.kt
+++ back/aboutme-app/src/main/kotlin/com/aboutme/app/common/util/CommonValidationUtil.kt
@@ -1,7 +1,7 @@
 package com.aboutme.app.common.util
 
-import com.aboutme.app.common.error.CommonErrorCode
 import com.aboutme.common.exception.GlobalException
+import com.aboutme.core.common.error.CommonErrorCode
 
 class CommonValidationUtil {
     companion object {
