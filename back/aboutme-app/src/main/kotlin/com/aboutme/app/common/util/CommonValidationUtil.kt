6f11e6e8668d8738f38321216a46f0de9a035544
LSH <lsh2613@naver.com>
2026-03-25 23:32:12 +0900

    refactor(back): 사용하지 않는 메서드 제거

--- back/aboutme-app/src/main/kotlin/com/aboutme/app/common/util/CommonValidationUtil.kt
+++ back/aboutme-app/src/main/kotlin/com/aboutme/app/common/util/CommonValidationUtil.kt
@@ -2,7 +2,6 @@ package com.aboutme.app.common.util
 
 import com.aboutme.app.common.error.CommonErrorCode
 import com.aboutme.common.exception.GlobalException
-import java.time.LocalDate
 
 class CommonValidationUtil {
     companion object {
@@ -12,17 +11,6 @@ class CommonValidationUtil {
             val sequentialSeq = (1..seq.size).toList()
             if (sortedSeq != sequentialSeq) {
                 throw GlobalException(CommonErrorCode.INVALID_SEQ)
-            }
-        }
-
-        fun validateChronologicalDate(
-            startDate: LocalDate,
-            endDate: LocalDate?,
-        ) {
-            endDate?.let {
-                if (it.isBefore(startDate)) {
-                    throw GlobalException(CommonErrorCode.INVALID_PERIOD)
-                }
             }
         }
     }
